60a96fafd0341c91420ef3c062c8ad95870a10a3
Lanerxx <892907311@qq.com>
2020-06-03 22:38:22 +0800

    Injection MethodValidationPostProcessor and PasswordEncoder

--- src/main/java/com/example/springbootgettutor/SpringbootGetTutorApplication.java
+++ src/main/java/com/example/springbootgettutor/SpringbootGetTutorApplication.java
@@ -1,10 +1,13 @@
 package com.example.springbootgettutor;
 
-import com.example.springbootgettutor.repository.BaseRepository;
 import com.example.springbootgettutor.repository.impl.BaseRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 
 @SpringBootApplication
 @EnableJpaRepositories(repositoryBaseClass = BaseRepositoryImpl.class)
@@ -14,4 +17,14 @@ public class SpringbootGetTutorApplication {
         SpringApplication.run(SpringbootGetTutorApplication.class, args);
     }
 
+    @Bean
+    public MethodValidationPostProcessor methodValidationPostProcessor() {
+        return new MethodValidationPostProcessor();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+
+    }
 }
