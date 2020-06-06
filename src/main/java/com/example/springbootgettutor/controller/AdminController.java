e379f7dacf4e9ed3fa6ad08aad3775594285416d
Lanerxx <892907311@qq.com>
2020-06-06 21:37:00 +0800

    add AdminController

---
+++ src/main/java/com/example/springbootgettutor/controller/AdminController.java
@@ -0,0 +1,56 @@
+package com.example.springbootgettutor.controller;
+
+import com.example.springbootgettutor.entity.Course;
+import com.example.springbootgettutor.entity.Tutor;
+import com.example.springbootgettutor.entity.User;
+import com.example.springbootgettutor.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/admin/")
+@Slf4j
+public class AdminController {
+    @Autowired
+    private PasswordEncoder encoder;
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("tutor")
+    public Map addTutor(@Valid @RequestBody User user){
+        Tutor tutor = new Tutor();
+        if(user.getNumber()!=null && user.getName()!=null){
+            User u = new User();
+            u.setNumber(user.getNumber());
+            u.setName(user.getName());
+            u.setPassword(encoder.encode(String.valueOf(user.getNumber())));
+            u.setRole(User.Role.TUTOR);
+            tutor.setUser(u);
+            userService.addTutot(tutor);
+        }
+        else{
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Name, number cannot be empty.");
+        }
+        return Map.of(
+                "tutor",tutor
+        );
+    }
+
+    @DeleteMapping("tutor/{tid}")
+    public Map deleteTotur(@PathVariable int tid){
+        if(userService.getTutorById(tid)==null){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "The tutor you want to delete does not exist.");
+        }
+        userService.deletTutor(tid);
+        return Map.of("massage", "Successful delete!");
+    }
+}
