1591160b650330a7a734e8aef0da9c5ae1da9be7
Lanerxx <892907311@qq.com>
2020-06-25 15:08:37 +0800

    Modify the method of batch adding course selection records: determine whether there is any course selection record before, and perform different operations

--- src/main/java/com/example/springbootgettutor/controller/TutorController.java
+++ src/main/java/com/example/springbootgettutor/controller/TutorController.java
@@ -46,8 +46,65 @@ public class TutorController {
         );
     }
 
+    //Student Information
+    @PostMapping("student")
+    public  Map addStudent(@Valid @RequestBody Student student){
+        Tutor tutor = userService.getTutorById(requestComponent.getUid());
+        if(userService.getUserByNumber(student.getUser().getNumber()) !=null){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "You have already added the student and cannot repeat it!");
+        }
+        User u = new User();
+        u.setName(student.getUser().getName());
+        u.setNumber(student.getUser().getNumber());
+        u.setRole(User.Role.STUDENT);
+        u.setPassword(encoder.encode(String.valueOf(student.getUser().getNumber())));
+        Student s = new Student();
+        s.setUser(u);
+        userService.addStudent(s);
+        return Map.of(
+                "student",s
+        );
+
+    }
+
+    @DeleteMapping("student/{sid}")
+    public Map deleteStudent(@PathVariable int sid) {
+        userService.deleteStudent(sid);
+        return Map.of(
+                "massage","successful delete!"
+        );
+    }
+
+    @PatchMapping("student")
+    public Map updateStudent(@Valid @RequestBody Student student) {
+        Tutor tutor = userService.getTutorById(requestComponent.getUid());
+        Student s = userService.getStudent(student.getId());
+        if(s ==null){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "The course you want to change doesn't exist");
+        }
+        User u = userService.getUserByID(student.getId());
+        u.setNumber(student.getUser().getNumber());
+        u.setName(student.getUser().getName());
+        userService.updateUser(u);
+        return Map.of(
+                "student",student
+        );
+    }
+
+    @GetMapping("students")
+    public Map getStudents(){
+        List<Student> students = userService.listStudents();
+        return Map.of(
+                "students",students
+        );
+    }
+
+
+
     //Course Information
-    @PostMapping("courses")
+    @PostMapping("course")
     public Map addCourse(@Valid @RequestBody Course course) {
         Tutor tutor = userService.getTutorById(requestComponent.getUid());
         List<Course> courses = classService.listCourseByTutorID(requestComponent.getUid());
@@ -78,6 +135,7 @@ public class TutorController {
     public Map updateCourse(@Valid @RequestBody Course course) {
         Tutor tutor = userService.getTutorById(requestComponent.getUid());
         Course c = classService.getCourse(course.getId());
+        log.debug("{}", course.getId());
         if(c == null){
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
                     "The course you want to change doesn't exist");
@@ -122,35 +180,62 @@ public class TutorController {
     public Map addElectives(@Valid @RequestBody List<Elective> electives){
         Tutor tutor = userService.getTutorById(requestComponent.getUid());
         electives.forEach(elective -> {
+            log.debug("{}", elective.getGrade());
             Student student = elective.getStudent();
             Course course = elective.getCourse();
-            Student s = userService.getStudentByUserNumber(elective.getStudent().getUser().getNumber());
-            Course c = classService.getCourse(course.getName(), tutor.getId());
             if(student == null || course == null){
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
                         "Students and classes cannot be empty");
             }
-            if(s == null){
-                User u = new User();
-                u.setName(student.getUser().getName());
-                u.setNumber(student.getUser().getNumber());
-                u.setRole(User.Role.STUDENT);
-                u.setPassword(encoder.encode(String.valueOf(student.getUser().getNumber())));
-                s = new Student();
-                s.setUser(u);
-                userService.addStudent(s);
+            Student s = userService.getStudentByUserNumber(elective.getStudent().getUser().getNumber());
+            Course c = classService.getCourse(course.getName(), tutor.getId());
+            //添加选课记录前，课程与学生都存在，还能查询选课记录是否存在
+            if(c!=null && s!=null){
+                Elective e = classService.getElectiveByStudentIdAndCourseId(s.getId(), c.getId());
+                log.debug("{}", e);
+                //选课记录存在时，相当于选课记录中的成绩更新
+                if(e !=null){
+                    e.setGrade(elective.getGrade());
+                    classService.updateElective(e);
+                }
+                //选课记录不存在时，直接添加新的选课记录
+                else {
+                    Elective e2 = new Elective();
+                    e2.setGrade(elective.getGrade());
+                    e2.setStudent(userService.getStudent(s.getId()));
+                    e2.setCourse(classService.getCourse(course.getName(), tutor.getId()));
+                    classService.addElective(e2);
+                }
             }
-            if(c == null){
-                c = new Course();
-                c.setName(course.getName());
-                c.setTutor(tutor);
-                classService.addCourse(c);
+            //添加选课记录前，课程与学生不存在
+            else {
+                //学生不存在，添加学生
+                if(s == null){
+                    User u = new User();
+                    u.setName(student.getUser().getName());
+                    u.setNumber(student.getUser().getNumber());
+                    u.setRole(User.Role.STUDENT);
+                    u.setPassword(encoder.encode(String.valueOf(student.getUser().getNumber())));
+                    s = new Student();
+                    s.setUser(u);
+                    userService.addStudent(s);
+                }
+                //课程不存在，添加课程，且设置默认权重为0.5，默认最低分为60
+                if(c == null){
+                    c = new Course();
+                    c.setName(course.getName());
+                    c.setTutor(tutor);
+                    c.setLowestMark(60);
+                    c.setWeight(0.5);
+                    classService.addCourse(c);
+                }
+                //选课记录必然不存在，添加选课记录
+                Elective e1 = new Elective();
+                e1.setGrade(elective.getGrade());
+                e1.setStudent(userService.getStudent(s.getId()));
+                e1.setCourse(classService.getCourse(course.getName(), tutor.getId()));
+                classService.addElective(e1);
             }
-            Elective e = new Elective();
-            e.setGrade(elective.getGrade());
-            e.setStudent(userService.getStudent(s.getId()));
-            e.setCourse(classService.getCourse(course.getName(), tutor.getId()));
-            classService.addElective(e);
         });
         return Map.of(
                 "electives",electives
@@ -219,10 +304,18 @@ public class TutorController {
 
     //Student Ranking
     @GetMapping("ranking")
-    public Map getStudents(){
+    public Map getRankingStudents(){
+        log.debug("{}", "sadasdas");
         List<Student> students = classService.RankStudents(requestComponent.getUid());
+        students.forEach(s->{
+            log.debug("{}", s.getId());
+            log.debug("{}", s.getUser().getName());
+            log.debug("{}", s.getWeightedGrade());
+            log.debug("{}", s.getUser().getNumber());
+                }
+        );
         return Map.of(
-                "student",students
+                "students",students
         );
     }
 
